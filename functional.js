861a80db78599ba069045aa93948c3b421082e0c
Andrew Greenfield <greenfield.andrew@gmail.com>
2019-06-28 20:22:57 +0000

    Section 6.3.1 reduce example

--- functional.js
+++ functional.js
@@ -28,3 +28,34 @@ function fullUrls(elements) {
   return elements.map( (element) => `htps://example.com/${urlify(element)}`);
 }
 console.log(fullUrls(states));
+
+// exercise 6.2.1 a filter function for Dakotas
+function findDakotas(elements) {
+  return elements.filter( element => element.toLowerCase().includes("dakota"));
+}
+console.log(findDakotas(states));
+
+function findDoubles(elements) {
+  return elements.filter( element => element.split(/\s+/).length === 2);
+}
+console.log(findDoubles(states));
+
+// REDUCE EXAMPLES
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//sum: Imperative solution
+
+function imperativeSum(elements) {
+  let total = 0;
+  numbers.forEach( function(n) {
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+function functionalSum (elements){
+  return elements.reduce((total, n) => { return total += n });
+}
+console.log(functionalSum(numbers));
